0edd39512ef766ac5b15796d8942051047ba3e5f
AlvaDamian <alva.damian@yahoo.com.ar>
2022-12-16 20:27:16 -0300

    Changed header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,35 @@
 import { useContext } from 'react';
+import styled from 'styled-components';
 import SubtotalContext from '../contexts/SubtotalContext';
+
+const HeaderComponent = styled.header`
+  background-color: #ffa500;
+`;
+
+const ValuesContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+const Value = styled.span`
+  margin: 0px 24px;
+  color: white;
+  font-size: 20px;
+`;
 
 export function Header() {
 
   const { subtotal, subtotalWithTaxes } = useContext(SubtotalContext);
 
   return (
-    <header style={{ background: 'red' }}>
+    <HeaderComponent>
       <img
         src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png"
         alt="logo"
       />
-      <div>SubTotal $ {subtotal}</div>
-      <div>SubTotal with taxes $ {subtotalWithTaxes}</div>
-    </header>
+      <ValuesContainer>
+        <Value>SubTotal $ {subtotal}</Value>
+        <Value>SubTotal with taxes $ {subtotalWithTaxes}</Value>
+      </ValuesContainer>
+    </HeaderComponent>
   );
 }
