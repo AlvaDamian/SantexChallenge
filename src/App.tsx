da6e17c0478e97435e7c724ec39e6edfca62be7c
AlvaDamian <alva.damian@yahoo.com.ar>
2022-12-16 20:16:37 -0300

    Subtotal is now persisted in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from 'react';
 import { Header } from './components/Header';
 import ProductList from './components/ProductList/ProductList';
 import SubtotalContext from './contexts/SubtotalContext';
+import useStateWithStorage from './hooks/useStateWithStorage';
 
 function App() {
 
-  const [ subtotal, setSubtotal ] = useState(0);
-  const [ subtotalWithTaxes, setSubtotalWithTaxes ] = useState(0);
+  const [ subtotal, setSubtotal ] = useStateWithStorage("subtotal", 0);
+  const [ subtotalWithTaxes, setSubtotalWithTaxes ] = useStateWithStorage("subtotalWithTaxes", 0);
 
   const addSale = (saleSubtotal:number, saleSubtotalWithTaxes:number) => {
     console.log('called add sale', saleSubtotal, saleSubtotalWithTaxes);
-    setSubtotal(prev => prev + saleSubtotal);
-    setSubtotalWithTaxes(prev => prev + saleSubtotalWithTaxes);
+    setSubtotal((prev:string | null) => (prev ? +prev : 0) + saleSubtotal);
+    setSubtotalWithTaxes((prev:string | null) => "" + ((prev ? +prev : 0) + saleSubtotalWithTaxes));
   }
 
   return (
     <SubtotalContext.Provider value={{
-      subtotal,
-      subtotalWithTaxes,
+      subtotal: +subtotal,
+      subtotalWithTaxes: +subtotalWithTaxes,
       addSale
     }}>
       <Header></Header>
